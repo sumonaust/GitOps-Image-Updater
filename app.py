9a672735041ad52825d6f8100df4b8f6cf2862e4
sumonaust <sumonaust22@gmail.com>
2026-06-04 02:10:15 +0600

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "GitOps + ArgoCD + Image Updater Running000!"
+    return "GitOps + ArgoCD + Image Updater Running!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
